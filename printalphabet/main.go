e01e6c2ef58c3fac191280ce00c2f08decc77729
ialali <ibrahim.alali94@yahoo.ro>
2023-02-22 16:49:01 +0000

    printreversealphabet 2

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,9 @@
 package main
 
-import "github.com/01-edu"
+import "github.com/01-edu/z01"
 
 func main() {
-	for j := 0; j <= '9'; j++ {
+	for j := '0'; j <= '9'; j++ {
 		z01.PrintRune(j)
 	}
 	z01.PrintRune('\n')
