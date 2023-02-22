61acadbd373bb3a04e59afcbcdf07318e56bbf41
ialali <ibrahim.alali94@yahoo.ro>
2023-02-22 15:53:05 +0000

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -3,11 +3,11 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	
-		if nb < 0 {
-			z01.PrintRune("T")
+		if nb >= 0 {
+			z01.PrintRune("F")
 		} 
 		else {
-			z01.PrintRune("F")
+			z01.PrintRune("T")
 		}
 		z01.PrintRune("/n")
 	
