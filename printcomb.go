1a199f868ed94e8918c11d248bb996e9fe4394e6
ialali <ibrahim.alali94@yahoo.ro>
2023-02-23 13:16:11 +0000

    printcombi.go

--- printcomb.go
+++ printcomb.go
@@ -5,13 +5,13 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for a := '0'; a <= '9'; a++ {
 		for b := a + 1; b <= '9'; b++ {
-			for c := b + 1; c <= 9; c++ {
+			for c := b + 1; c <= '9'; c++ {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
 				if a < 55 {
-					z01.PrintRune(44)
-					z01.PrintRune(32)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
 			}
 		}
