07fc05850e971eaef9fb74e2975f5caf710f9acc
ialali <125916230+ialali@users.noreply.github.com>
2023-02-27 23:08:21 +0000

    recursivefactorial.go

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 {
+	if nb == 0 {
 		return 1
+	} else if nb < 0 || nb > 20 {
+		return 0
+	} else {
+		return nb * RecursiveFactorial(nb-1)
 	}
-	if nb > 1 {
-		return RecursiveFactorial(nb-1) * RecursiveFactorial(nb-1)
-	}
-	return 0
 }
---
+++ test/test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	arg := 0
+	fmt.Println(piscine.RecursiveFactorial(arg))
+}
