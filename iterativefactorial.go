bb9989d147b970feeabd3dfa39ea5ce97ba6c903
ialali <125916230+ialali@users.noreply.github.com>
2023-02-27 22:15:24 +0000

    iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb >= 0 && nb <= 20 {
 		f := 1
-		for i := 1; i <= nb; i++ {
+		for i := 1; i <= 20; i++ {
 			f = f * i
 		}
 		return (f)
