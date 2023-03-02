317737dac8b5c2994706877495edfd3ae52d7d16
ialali <125916230+ialali@users.noreply.github.com>
2023-03-02 13:42:49 +0000

    main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"os"
+
 	"github.com/01-edu/z01"
 )
+
 func main() {
 	name := os.Args[0]
-	for _, r := range name {
-		z01.PrintRune(r)
+	for _, val := range name {
+		z01.PrintRune(val)
 	}
 	z01.PrintRune('\n')
 }
