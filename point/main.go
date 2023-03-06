eb665b6adc92d8c5c5e2ea2b9e7c766fdeb2f02f
ialali <125916230+ialali@users.noreply.github.com>
2023-03-06 15:07:03 +0000

    point

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -14,19 +16,42 @@ func setPoint(ptr *point) {
 
 func main() {
 	points := &point{}
+
 	setPoint(points)
 
 	z01.PrintRune('x')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune(points.x + 48))
+	printInt(points.x)
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
 	z01.PrintRune('y')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune(points.y + 48))
+	printInt(points.y)
 	z01.PrintRune('\n')
+}
+
+func printInt(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+
+	arr := []rune{}
+	for n > 0 {
+		arr = append(arr, rune((n%10)+48))
+		n /= 10
+	}
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		z01.PrintRune(arr[i])
+	}
 }
