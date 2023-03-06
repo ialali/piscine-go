2b61c8ebf72a123c0b158d08c7640f205e38ba65
ialali <125916230+ialali@users.noreply.github.com>
2023-03-06 10:30:20 +0000

    appendrange

---
+++ appendrange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	if min <= max {
+		return nil
+	}
+	slice := []int{}
+	for i := min; i < max; i++ {
+		slice = append(slice, i)
+	}
+	return slice
+}
