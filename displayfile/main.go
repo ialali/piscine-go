aff0b1ad90f8cc7249d67a4a4f2b60cf0f8c9cb9
ialali <125916230+ialali@users.noreply.github.com>
2023-03-07 11:51:51 +0000

    displayfile

--- displayfile/main.go
+++ displayfile/main.go
@@ -12,6 +12,11 @@ func main() {
 		return
 	}
 
+	if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+
 	filename := os.Args[1]
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
