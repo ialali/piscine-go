f4cc20af834fc7e22086c0ccd3f8cdbf7a72cf58
ialali <125916230+ialali@users.noreply.github.com>
2023-03-07 15:50:38 +0000

    issorted.go"

--- issorted.go
+++ issorted.go
@@ -1,12 +1,9 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 1; i < len(a); i++ {
-		cmp := f(a[i-1], a[i])
-		if cmp > 0 {
-			return false
-		}
-		if cmp == 0 && i > 1 && f(a[i-2], a[i-1]) != 0 {
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
 			return false
 		}
 	}
