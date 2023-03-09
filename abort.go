be8c5337fa60c4de16a2c0863ed6d1a48fa1c66d
ialali <125916230+ialali@users.noreply.github.com>
2023-03-09 11:04:01 +0000

    abort.go

--- abort.go
+++ abort.go
@@ -1,37 +1,19 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	min := a
-	if b < min {
-		min = b
-	}
-	if c < min {
-		min = c
-	}
-	if d < min {
-		min = d
-	}
-	if e < min {
-		min = e
+	arr := []int{a, b, c, d, e}
+
+	// Sort the array of integers in ascending order
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
 	}
 
-	max := a
-	if b > max {
-		max = b
-	}
-	if c > max {
-		max = c
-	}
-	if d > max {
-		max = d
-	}
-	if e > max {
-		max = e
-	}
+	// Calculate the median
+	median := arr[2]
 
-	sum := a + b + c + d + e
-
-	middleSum := sum - min - max
-
-	return middleSum / 3
+	return median
 }
---
+++ test/test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	middle := piscine.Abort(1, 2, 3, 4, 5)
+	fmt.Println(middle)
+}
