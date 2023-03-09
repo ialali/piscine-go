d21c45eb6b25fceb0444241028e732f5c85d2e1c
ialali <125916230+ialali@users.noreply.github.com>
2023-03-09 10:48:35 +0000

    abort.go

--- abort.go
+++ abort.go
@@ -1,9 +1,35 @@
 package piscine
 
-import "sort"
-
 func Abort(a, b, c, d, e int) int {
-	arr := []int{a, b, c, d, e}
-	sort.Ints(arr)
-	return arr[2]
+	sum := a + b + c + d + e
+	max := a
+	min := a
+
+	// Find the maximum and minimum values among the arguments
+	if b > max {
+		max = b
+	}
+	if c > max {
+		max = c
+	}
+	if d > max {
+		max = d
+	}
+	if e > max {
+		max = e
+	}
+
+	if b < min {
+		min = b
+	}
+	if c < min {
+		min = c
+	}
+	if d < min {
+		min = d
+	}
+	if e < min {
+		min = e
+	}
+	return (sum - max - min) / 3
 }
